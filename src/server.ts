399ab3083034f6d64a34f28c5b224a792d64cf73
diovanedm <diovane.dm@gmail.com>
2022-03-05 11:51:27 -0300

    Debug aplication

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     return res.send('Hello World! Welcome to Node course')
 }) 
+
+app.post("/courses", (req, res) => {
+    const { name } = req.body;
+    return res.json({ name });
+})
 
 app.listen(3333, () => console.log("Server is running!"))
